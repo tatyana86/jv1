5c4e424f8e90f52028caa796612a86c6c76d85b9
tatyana86 <Krivonogova_1999@mail.ru>
2023-05-11 01:21:51 +0400

    not ready, just try

---
+++ oopPart1.java
@@ -0,0 +1,21 @@
+1.1.
+a) в приложении для чтения книг может быть реализован класс book с такими полями: автор (String), название (String), год издания (int), количество страниц (int), рейтинг (double).
+б) в приложении для покупки товаров может быть реализован класс product с такими полями: тип товара (String), цвет (String), стоимость (double), наличие (boolean).
+в) в игре для персонажа может быть реализован класс woman с полями: имя (String), возраст (int), цвет волос (String), цвет глаз (String).
+1.2.
+// наш класс
+class Dwarf {
+  String name;
+  int age;
+  double beardLen;  // длина бороды
+  String labor; // род деятельности
+  String religion; // уровень поклонения 
+  boolean marriage; // признак женитьбы
+}
+
+
+public class YourClassNameHere {
+    public static void main(String[] args) {
+
+    }
+}
