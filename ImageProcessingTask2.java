06554fb112647457d7d4b559c6d4132a7526a112
tatyana86 <Krivonogova_1999@mail.ru>
2023-06-05 23:10:39 +0400

    add solution

---
+++ ImageProcessingTask2.java
@@ -0,0 +1,65 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ImageProcessingTask2 {
+
+    public static boolean ConvImageExt(String extOld, String extNew)  {
+        File root = new File(".\\"); // связываем переменную root с текущей директорией
+        ArrayList<File> expand = new ArrayList<File>(); // массив для файлов и каталогов в текущей директории
+        expand.addAll(Arrays.asList(root.listFiles())); // добавляем все файлы и подкаталоги полученной директории
+
+        try {
+            // Проходимся по содержимому директории:
+            for (File file : expand) {
+                String fileName = file.getName(); // запишем название файла
+                // В названии есть переданное расширение?
+                if(fileName.contains("." + extOld)) {
+                    // Создаем переменную с изображением нужного расширения и холст:
+                    BufferedImage image = ImageIO.read(file);
+                    Graphics2D g = image.createGraphics();
+
+                    int a = 150; // сторона квадрата
+                    g.setColor(Color.BLUE); // задаем цвет квадрата
+                    // Рисуем квадрат:
+                    g.drawRect((image.getWidth()-a)/2, (image.getHeight()-a)/2, a, a);
+
+                    g.setColor(Color.BLACK); // задаем цвет текста
+                    Font font = new Font("Arial", Font.BOLD|Font.ITALIC, 30); // создаем шрифт и его свойства
+                    g.setFont(font); // устанавливаем созданный шрифт текущим
+                    // Пишем текст:
+                    g.drawString("Hello,", image.getWidth()/2 - 45,image.getHeight()/2);
+                    g.drawString("World!", image.getWidth()/2 - 45,image.getHeight()/2 + 30);
+
+                    g.dispose(); // освобождаем холст
+
+                    // Сохраняем изображение с новым расширением:
+                    ImageIO.write(image, extNew, new File(".\\" + fileName.substring(0, fileName.indexOf(".")) + "." + extNew));
+                    // Удаляем файл со старым расширением:
+                    file.delete();
+                }
+            }
+            return true;
+        } catch (Exception e) {
+            System.out.println("Возникла проблема: " + e);
+            return false;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        String ext1 = "jpg"; // искомое расширение
+        String ext2 = "png"; // новое расширение
+
+        if(ConvImageExt(ext1, ext2))
+            System.out.println("Все найденные изображения ." + ext1 + " переконвертированы в ." + ext2 + ".");
+        else
+            System.out.println("Не удалось выполнить конвертацию.");
+        
+    }
+}
