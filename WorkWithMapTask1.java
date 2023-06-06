d0134501b51d3e8cc913cbd8620e268bda6166d1
tatyana86 <Krivonogova_1999@mail.ru>
2023-06-06 21:57:31 +0400

    add solution

---
+++ WorkWithMapTask1.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Random;
+
+public class WorkWithMapTask1 {
+	
+    public static void main(String[] args) {
+		
+		HashMap<Integer, String> map100 = new HashMap<Integer, String>(); // объявляем HashMap
+        Random rand = new Random();
+        int n = 100;
+        int[] keys = new int[n];
+        int i = 0;
+        // Заполняем массив случайными уникальными числами:
+        while(i < n) {
+            int r = rand.nextInt(1000) + 1;
+            boolean isRepeat = false; // проверка, встречалось ли уже такое число
+            for(int j = 0; j < i; j ++)
+                if(keys[j] == r) {
+                    isRepeat = true;
+                    break;
+                }
+            if(isRepeat) { // если такое число встречалось, генерируем новое случаное число
+                continue;
+            }
+            keys[i] = r; // нашли оригинальное число
+            i ++;
+        }
+
+        // Добавляем 100 случайных пар в словарь:
+        for(int k = 0; k < 100; k ++) {
+            map100.put(keys[k], String.valueOf(keys[k]));
+        }
+
+        // Выведем значения по ключам:
+        for(Integer k : map100.keySet()) {
+            System.out.println(map100.get(k));
+        }
+
+        // Удаляем все пары:
+        map100.clear();
+
+    }
+}
---
+++ WorkWithMapTask2.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+public class WorkWithMapTask2 {
+	
+    static ArrayList<Integer> repeatNum(int [] list, int N) {
+
+        ArrayList<Integer> res = new ArrayList<>(); // результирующий список
+        HashMap<Integer, Integer> numCount = new HashMap<Integer, Integer>(); // словарь для подсчета чисел в переданном списке
+        
+		// Посчитаем повторения чисел в переданном списке:
+        for(int i = 0; i < list.length; i ++) {
+            // Метод .getOrDefault() возвращает значение по ключу, если таковой существует;
+            // в противном случае создает пару с переданным ключом и присваивает значение второго аргумента,
+            // в данном случае - 0.
+            numCount.put(list[i], numCount.getOrDefault(list[i], 0) + 1);
+        }
+
+        // Пройдемся по парам словаря и добавим в результирующий список числа, повторяющиеся не менее N раз:
+        for(Integer k : numCount.keySet()) {
+            if(numCount.get(k) >= N) {
+                res.add(k);
+            }
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+
+        int Num = 10; // введем число повторений
+        Random rand = new Random();
+        int n = 100;
+        int[] randNums = new int[n];
+
+        // Заполняем массив случайными числами:
+        for(int i = 0; i < 100; i ++) {
+            randNums[i] = rand.nextInt(10) + 1;
+        }
+
+        ArrayList<Integer> repeatN = repeatNum(randNums, Num);
+        
+    }
+}
